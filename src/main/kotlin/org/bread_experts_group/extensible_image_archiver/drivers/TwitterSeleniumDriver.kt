a14fae2e933072cb921fed3bb7e85374ad321456
LawsOfScience <thesandwichsupreme@gmail.com>
2025-02-10 16:13:48 -0500

    Add waits for login text boxes to load

--- src/main/kotlin/org/bread_experts_group/extensible_image_archiver/drivers/TwitterSeleniumDriver.kt
+++ src/main/kotlin/org/bread_experts_group/extensible_image_archiver/drivers/TwitterSeleniumDriver.kt
@@ -3,18 +3,25 @@ package org.bread_experts_group.extensible_image_archiver.drivers
 import org.bread_experts_group.extensible_image_archiver.SeleniumDriver
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
 import java.nio.file.Path
+import java.time.Duration
 
 class TwitterSeleniumDriver(usernameToSearch: String, private val twitterUsername: String, private val twitterPassword: String, override val seleniumDriver: WebDriver) : SeleniumDriver {
     override val url: String = "https://x.com/$usernameToSearch"
 
     override fun run(outputDirectory: Path) {
         this.seleniumDriver.get("https://x.com/i/flow/login")
-        this.seleniumDriver.findElement(By.cssSelector("input[autocomplete=username]"))
+
+        WebDriverWait(this.seleniumDriver, Duration.ofSeconds(5))
+            .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[autocomplete=username]")))
             .sendKeys(this.twitterUsername)
         this.seleniumDriver.findElement(By.xpath("\"//button/div/span/span[.='Next']\""))
             .click()
-        this.seleniumDriver.findElement(By.cssSelector("input[autocomplete=current-password]"))
+
+        WebDriverWait(this.seleniumDriver, Duration.ofSeconds(5))
+            .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[autocomplete=current-password]")))
             .sendKeys(this.twitterPassword)
         this.seleniumDriver.findElement(By.xpath("\"//button/div/span/span[.='Log in']\""))
             .click()
