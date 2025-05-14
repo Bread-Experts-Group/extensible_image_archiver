811106861c50e39c887bb0e71abca5d9fb222940
LawsOfScience <thesandwichsupreme@gmail.com>
2025-05-14 13:18:25 -0400

    Add handling for manga galleries

--- src/main/kotlin/org/bread_experts_group/extensible_image_archiver/drivers/PixivSeleniumDriver.kt
+++ src/main/kotlin/org/bread_experts_group/extensible_image_archiver/drivers/PixivSeleniumDriver.kt
@@ -4,9 +4,9 @@ import org.bread_experts_group.extensible_image_archiver.SeleniumDriver
 import org.openqa.selenium.By
 import org.openqa.selenium.JavascriptExecutor
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WindowType
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
-import java.lang.Thread.sleep
 import java.nio.file.Path
 import java.time.Duration
 
@@ -17,24 +17,28 @@ class PixivSeleniumDriver(
     override val url = "https://pixiv.net/en/users/$userIdToSearch/illustrations"
 
     override fun run(outputDirectory: Path) {
-        println("URL: $url")
         this.seleniumDriver.get(this.url)
         WebDriverWait(this.seleniumDriver, Duration.ofSeconds(5))
-            .until(ExpectedConditions
-                .presenceOfElementLocated(By.className("thumbnail-link")))
+            .until(
+                ExpectedConditions
+                    .presenceOfElementLocated(By.className("thumbnail-link"))
+            )
 
         val images = this.seleniumDriver
             .findElements(By.className("thumbnail-link"))
-
-        println("Found ${images.size} images")
+        this.seleniumDriver.windowHandle
 
         for (image in images) {
             val anchorHref = image.getDomAttribute("href")
+            this.seleniumDriver.switchTo().newWindow(WindowType.TAB)
+
             this.seleniumDriver.get("https://pixiv.net$anchorHref")
 
             val src = WebDriverWait(this.seleniumDriver, Duration.ofSeconds(5))
-                .until(ExpectedConditions
-                    .presenceOfElementLocated(By.className("work-thumb")))
+                .until(
+                    ExpectedConditions
+                        .presenceOfElementLocated(By.className("work-thumb"))
+                )
                 .getDomAttribute("src")
 
             if (src == null) {
@@ -44,17 +48,26 @@ class PixivSeleniumDriver(
 
             val mangaButton = this.seleniumDriver.findElements(By.xpath("//button[text()='Show all']"))
             if (mangaButton.size == 1) {
-                println("Manga url")
-            } // else {}  // handle single image
+                mangaButton[0].click()
+                WebDriverWait(this.seleniumDriver, Duration.ofSeconds(5))
+                    .until(
+                        ExpectedConditions.presenceOfElementLocated(
+                            By.className("manga-pages")
+                        )
+                    )
 
-            (this.seleniumDriver as JavascriptExecutor).executeScript("window.open('${src}', '_blank');")
-            val newWindow = this.seleniumDriver.windowHandles
-                .filter { it != this.seleniumDriver.windowHandle }[0]
-            this.seleniumDriver.switchTo().window(newWindow)
-
-            sleep(2000)
-            println("Image src: ${this.seleniumDriver.currentUrl}")
-            break
+                val pages = this.seleniumDriver.findElements(By.className("manga-page"))
+                for (page in pages) {
+                    val imageSrc = page
+                        .findElement(By.xpath("./div/img"))
+                        .getDomAttribute("src")
+                    println(imageSrc)
+                }
+            } else {
+                (this.seleniumDriver as JavascriptExecutor).executeScript("window.open('${src}', '_blank');")
+                val newWindow = this.seleniumDriver.windowHandles.filter { it != this.seleniumDriver.windowHandle }[0]
+                this.seleniumDriver.switchTo().window(newWindow)
+            }
         }
     }
 }
